8bdca019b42802448ee660180e9661ddfc422850
vihanrs <vihanvrs@gmail.com>
2024-08-01 22:42:36 +0530

    developed daily extra income module

---
+++ src/main/java/com/jenakahw/controller/DailyExtraIncomeController.java
@@ -0,0 +1,143 @@
+package com.jenakahw.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jenakahw.domain.DailyExtraIncome;
+import com.jenakahw.repository.DailyExtraIncomeRepository;
+import com.jenakahw.repository.DailyIncomeExpensesStatusRepository;
+
+@RestController
+//add class level mapping /dailyextraincome
+@RequestMapping(value = "/dailyextraincome")
+public class DailyExtraIncomeController {
+	/*
+	 * Create Repository object -> Dependency injection:Repository is an interface
+	 * so it cannot create instance then use dependency injection
+	 */
+	@Autowired
+	private DailyExtraIncomeRepository dailyExtraIncomeRepository;
+	
+	@Autowired
+	private DailyIncomeExpensesStatusRepository dailyIncomeExpensesStatusRepository;
+
+	@Autowired
+	private PrivilegeController privilegeController;
+
+	@Autowired
+	private UserController userController;
+
+	private static final String MODULE = "Daily Extra Income";
+
+	// get mapping for generate daily extra income UI
+	@GetMapping
+	public ModelAndView getDailyExtraIncomeUI() {
+		// get logged user authentication object
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		ModelAndView dailyextraincomeView = new ModelAndView();
+		dailyextraincomeView.addObject("title", "Daily Extra Income  | Jenaka Hardware");
+		dailyextraincomeView.addObject("logusername", auth.getName());
+		dailyextraincomeView.setViewName("dailyextraincome.html");
+		return dailyextraincomeView;
+	}
+
+	// get service mapping for get all daily extra incomes
+	@GetMapping(value = "/findall", produces = "application/json")
+	public List<DailyExtraIncome> findAll() {
+		// check privileges
+		if (privilegeController.hasPrivilege(MODULE, "select")) {
+			return dailyExtraIncomeRepository.findAll(Sort.by(Direction.DESC, "id"));
+		} else {
+			return null;
+		}
+
+	}
+
+	// post mapping for save new daily extra income
+	@PostMapping
+	public String saveDailyExtraIncome(@RequestBody DailyExtraIncome dailyExtraIncome) {
+		// check privileges
+		if (!privilegeController.hasPrivilege(MODULE, "insert")) {
+			return "Access Denied !!!";
+		}
+
+		try {
+			// set added date time
+			dailyExtraIncome.setAddedDateTime(LocalDateTime.now());
+			// set added user
+			dailyExtraIncome.setAddedUserId(userController.getLoggedUser().getId());
+
+			dailyExtraIncomeRepository.save(dailyExtraIncome);
+			return "OK";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+
+	// post mapping for update daily extra income
+	@PutMapping
+	public String updateCustomer(@RequestBody DailyExtraIncome dailyExtraIncome) {
+		// check privileges
+		if (!privilegeController.hasPrivilege(MODULE, "update")) {
+			return "Access Denied !!!";
+		}
+
+		try {
+			// set added date time
+			dailyExtraIncome.setLastUpdatedDateTime(LocalDateTime.now());
+			// set added user
+			dailyExtraIncome.setUpdatedUserId(userController.getLoggedUser().getId());
+
+			dailyExtraIncomeRepository.save(dailyExtraIncome);
+			return "OK";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+
+	// delete mapping for delete daily extra income
+	@DeleteMapping
+	public String deleteProduct(@RequestBody DailyExtraIncome dailyExtraIncome) {
+		// check privileges
+		if (!privilegeController.hasPrivilege(MODULE, "delete")) {
+			return "Access Denied !!!";
+		}
+
+		// check given product exist or not
+		DailyExtraIncome extDailyExIncome = dailyExtraIncomeRepository.getReferenceById(dailyExtraIncome.getId());
+		if (extDailyExIncome == null) {
+			return "Daily Extra Income Not Exist..!";
+		}
+
+		try {
+			// set deleted data and time
+			dailyExtraIncome.setDeletedDateTime(LocalDateTime.now());
+
+			// set deleted user id
+			dailyExtraIncome.setDeletedUserId(userController.getLoggedUser().getId());
+
+			// set customer statuts to 'Deleted'
+			dailyExtraIncome.setDailyIncomeExpensesStatusId(dailyIncomeExpensesStatusRepository.getReferenceById(2));
+
+			dailyExtraIncomeRepository.save(dailyExtraIncome);
+			return "OK";
+		} catch (Exception e) {
+			return e.getMessage();
+		}
+	}
+}
---
+++ src/main/resources/static/resources/controller_js/dailyextraincome.js
@@ -0,0 +1,429 @@
+//Access Browser onload event
+window.addEventListener("load", () => {
+  //get logged user privileges
+  userPrivilages = ajaxGetRequest(
+    "/privilege/byloggeduserandmodule/Daily Extra Income"
+  );
+
+  //refresh all
+  refreshAll();
+
+  //set default selected section
+  if (userPrivilages.insert) {
+    showDefaultSection("addNewButton", "addNewSection");
+  } else {
+    showDefaultSection("viewAllButton", "viewAllSection");
+    addAccordion.style.display = "none";
+  }
+  //call all event listners
+  addEventListeners();
+});
+
+// ********* LISTENERS *********
+const addEventListeners = () => {
+  let numberWithdecimals = "^(([1-9]{1}[0-9]{0,7})|([0-9]{0,8}[.][0-9]{2}))$";
+
+  textAmount.addEventListener("keyup", () => {
+    textFieldValidator(
+      textAmount,
+      numberWithdecimals,
+      "dailyexincome",
+      "total"
+    );
+  });
+
+  textReason.addEventListener("keyup", () => {
+    textFieldValidator(textReason, "", "dailyexincome", "reason");
+  });
+
+  selectStatus.addEventListener("change", () => {
+    selectDFieldValidator(
+      selectStatus,
+      "dailyexincome",
+      "dailyIncomeExpensesStatusId"
+    );
+  });
+
+  //form reset button function call
+  btnReset.addEventListener("click", () => {
+    refreshForm();
+  });
+
+  //record update function call
+  btnUpdate.addEventListener("click", () => {
+    updateRecord();
+  });
+
+  //record save function call
+  btnAdd.addEventListener("click", () => {
+    addRecord();
+  });
+
+  //record print function call
+  btnViewPrint.addEventListener("click", () => {
+    printViewRecord();
+  });
+
+  //print full table function call
+  btnPrintFullTable.addEventListener("click", () => {
+    printFullTable();
+  });
+};
+
+// ********* RESET *********
+//function for refresh form and table
+const refreshAll = () => {
+  //Call form refresh function
+  refreshForm();
+  //Call table refresh function
+  refreshTable();
+};
+
+// ********* FORM OPERATIONS *********
+
+//function for refresh form area
+const refreshForm = () => {
+  //create empty object
+  dailyexincome = {};
+
+  // get status
+  statuses = ajaxGetRequest("/dailyincomeexpensesstatus/findall");
+  fillDataIntoSelect(selectStatus, "Select Status", statuses, "name", "Saved");
+
+  //bind default selected status in to supplier object and set valid color
+  dailyexincome.dailyIncomeExpensesStatusId = JSON.parse(selectStatus.value);
+  selectStatus.style.border = "2px solid #00FF7F";
+
+  //empty all elements
+  textAmount.value = "";
+  textReason.value = "";
+
+  //set default border color
+  let elements = [textAmount, textReason];
+  setBorderStyle(elements);
+
+  //manage form buttons
+  manageFormButtons("insert", userPrivilages);
+};
+
+//function for check errors
+const checkErrors = () => {
+  //need to check all required property fields
+  let error = "";
+
+  if (dailyexincome.total == null) {
+    error = error + "Please Enter Customer Name...!\n";
+    textAmount.style.border = "1px solid red";
+  }
+  if (dailyexincome.reason == null) {
+    error = error + "Please Enter Customer Contact No...!\n";
+    textReason.style.border = "1px solid red";
+  }
+
+  if (dailyexincome.dailyIncomeExpensesStatusId == null) {
+    error = error + "Please Select Valid Status...!\n";
+    selectStatus.style.border = "1px solid red";
+  }
+
+  return error;
+};
+
+//function for check updates
+const checkUpdates = () => {
+  let updates = "";
+
+  if (olddailyexincome.total != dailyexincome.total) {
+    updates +=
+      "Amount has changed " +
+      olddailyexincome.total +
+      " into " +
+      dailyexincome.total +
+      " \n";
+  }
+  if (olddailyexincome.reason != dailyexincome.reason) {
+    updates +=
+      "Reason No. has changed " +
+      olddailyexincome.reason +
+      " into " +
+      dailyexincome.reason +
+      " \n";
+  }
+
+  if (
+    olddailyexincome.dailyIncomeExpensesStatusId.id !=
+    dailyexincome.dailyIncomeExpensesStatusId.id
+  ) {
+    updates +=
+      "Status has changed " +
+      olddailyexincome.dailyIncomeExpensesStatusId.name +
+      " into " +
+      dailyexincome.dailyIncomeExpensesStatusId.name +
+      " \n";
+  }
+
+  return updates;
+};
+
+//function for add record
+const addRecord = () => {
+  //check form errors -
+  let formErrors = checkErrors();
+  if (formErrors == "") {
+    //get user confirmation
+    let title = "Are you sure to add following record..?\n";
+    let message =
+      "Amount (Rs.): " +
+      dailyexincome.total +
+      "\nReason : " +
+      dailyexincome.reason;
+    showConfirm(title, message).then((userConfirm) => {
+      if (userConfirm) {
+        //pass data into back end
+        let serverResponse = ajaxRequestBody(
+          "/dailyextraincome",
+          "POST",
+          dailyexincome
+        ); // url,method,object
+
+        //check back end response
+        if (serverResponse == "OK") {
+          showAlert("success", "Daily Extra Income Save successfully..!").then(
+            () => {
+              //need to refresh table and form
+              refreshAll();
+            }
+          );
+        } else {
+          showAlert(
+            "error",
+            "Daily Extra Income save not successfully..! have some errors \n" +
+              serverResponse
+          );
+        }
+      }
+    });
+  } else {
+    showAlert("error", formErrors);
+  }
+};
+
+//function for update record
+const updateRecord = () => {
+  let errors = checkErrors();
+  if (errors == "") {
+    let updates = checkUpdates();
+    if (updates != "") {
+      let title = "Are you sure you want to update following changes...?";
+      let message = updates;
+      showConfirm(title, message).then((userConfirm) => {
+        if (userConfirm) {
+          let serverResponse = ajaxRequestBody(
+            "/dailyextraincome",
+            "PUT",
+            dailyexincome
+          );
+          if (serverResponse == "OK") {
+            showAlert(
+              "success",
+              "Daily Extra Income Update successfully..!"
+            ).then(() => {
+              //need to refresh table and form
+              refreshAll();
+            });
+          } else {
+            showAlert(
+              "error",
+              "Daily Extra Income update not successfully..! have some errors \n" +
+                serverResponse
+            );
+          }
+        }
+      });
+    } else {
+      showAlert("warning", "Nothing to Update...!");
+    }
+  } else {
+    showAlert("error", "Cannot update!!!\n" + errors);
+  }
+};
+
+// ********* TABLE OPERATIONS *********
+
+//function for refresh table records
+const refreshTable = () => {
+  //array for store data list
+  dailyextraincomes = ajaxGetRequest("/dailyextraincome/findall");
+
+  //object count = table column count
+  //String - number/string/date
+  //function - object/array/boolean
+  //currency - RS
+  const displayProperties = [
+    { property: "total", datatype: "currency" },
+    { property: "reason", datatype: "String" },
+    { property: getAddedDate, datatype: "function" },
+    { property: getStatus, datatype: "function" },
+  ];
+
+  //call the function (tableID,dataList,display property list, view function name, refill function name, delete function name, button visibilitys, user privileges)
+  fillDataIntoTable(
+    dailyExtraIncomeTable,
+    dailyextraincomes,
+    displayProperties,
+    viewRecord,
+    refillRecord,
+    deleteRecord,
+    true,
+    userPrivilages
+  );
+
+  //hide delete button when status is 'deleted'
+  dailyextraincomes.forEach((dailyExtraIncome, index) => {
+    if (
+      userPrivilages.delete &&
+      dailyExtraIncome.dailyIncomeExpensesStatusId.name == "Deleted"
+    ) {
+      //catch the button
+      let targetElement =
+        customerTable.children[1].children[index].children[5].children[
+          userPrivilages.update && userPrivilages.insert ? 2 : 1
+        ];
+      //add changes
+      targetElement.style.pointerEvents = "none";
+      targetElement.style.visibility = "hidden";
+    }
+  });
+
+  $("#dailyExtraIncomeTable").dataTable();
+};
+
+// function for get status
+const getStatus = (rowObject) => {
+  if (rowObject.dailyIncomeExpensesStatusId.name == "Saved") {
+    return (
+      '<p class = "status status-active">' +
+      rowObject.dailyIncomeExpensesStatusId.name +
+      "</p>"
+    );
+  } else if (rowObject.dailyIncomeExpensesStatusId.name == "Deleted") {
+    return (
+      '<p class = "status status-error">' +
+      rowObject.dailyIncomeExpensesStatusId.name +
+      "</p>"
+    );
+  }
+};
+
+const getAddedDate = (rowObject) => {
+  return rowObject.addedDateTime.split("T")[0];
+};
+
+//function for view record
+const viewRecord = (rowObject, rowId) => {
+  //need to get full object
+  let printObj = rowObject;
+
+  tdAmount.innerText = "Rs." + parseFloat(printObj.total).toFixed(2);
+  tdReason.innerText = printObj.reason;
+  tdAddedDate.innerText = printObj.addedDateTime.split("T")[0];
+  tdStatus.innerText = printObj.dailyIncomeExpensesStatusId.name;
+
+  //open model
+  $("#modelDetailedView").modal("show");
+};
+
+//function for refill record
+const refillRecord = (rowObject, rowId) => {
+  $("#addNewButton").click();
+
+  dailyexincome = JSON.parse(JSON.stringify(rowObject)); //convert rowobject to json string and covert back it to js object
+  olddailyexincome = JSON.parse(JSON.stringify(rowObject)); // deep copy - create compeletely indipended two objects
+
+  textReason.value = dailyexincome.reason;
+  textAmount.value = dailyexincome.total;
+
+  // set status
+  fillDataIntoSelect(
+    selectStatus,
+    "Select Status",
+    statuses,
+    "name",
+    dailyexincome.dailyIncomeExpensesStatusId.name
+  );
+
+  setBorderStyle([selectStatus, textAmount, textReason]);
+
+  //manage buttons
+  manageFormButtons("refill", userPrivilages);
+};
+
+// //function for delete record
+const deleteRecord = (rowObject, rowId) => {
+  //get user confirmation
+  let title = "Are you sure!\nYou wants to delete following record? \n";
+  let message =
+    "Amount (Rs.): " + rowObject.total + "\nReason :" + rowObject.reason;
+
+  showConfirm(title, message).then((userConfirm) => {
+    if (userConfirm) {
+      //response from backend ...
+      let serverResponse = ajaxRequestBody(
+        "/dailyextraincome",
+        "DELETE",
+        rowObject
+      ); // url,method,object
+      //check back end response
+      if (serverResponse == "OK") {
+        showAlert("success", "Daily Extra Income Delete successfully..!").then(
+          () => {
+            // Need to refresh table and form
+            refreshAll();
+          }
+        );
+      } else {
+        showAlert(
+          "error",
+          "Daily Extra Income delete not successfully..! have some errors \n" +
+            serverResponse
+        );
+      }
+    }
+  });
+};
+
+// ********* PRINT OPERATIONS *********
+
+//print function
+const printViewRecord = () => {
+  newTab = window.open();
+  newTab.document.write(
+    //  link bootstrap css
+    "<head><title>Print Daily Extra Income</title>" +
+      '<link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css" /></head>' +
+      "<h2 style = 'font-weight:bold'>Daily Extra Income Details</h2>" +
+      printTable.outerHTML
+  );
+
+  //triger print() after 1000 milsec time out - time to load content to the printing tab
+  setTimeout(function () {
+    newTab.print();
+  }, 1000);
+};
+
+//print all data table after 1000 milsec of new tab opening () - to refresh the new tab elements
+const printFullTable = () => {
+  const newTab = window.open();
+  newTab.document.write(
+    //  link bootstrap css
+    "<head><title>Print Daily Extra Incomes</title>" +
+      '<script src="resources/js/jquery.js"></script>' +
+      '<link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css" /></head>' +
+      "<h2 style = 'font-weight:bold'>Daily Extra Incomes Details</h2>" +
+      dailyExtraIncomeTable.outerHTML +
+      '<script>$(".modify-button").css("display","none")</script>'
+  );
+
+  setTimeout(function () {
+    newTab.print();
+  }, 1000);
+};
