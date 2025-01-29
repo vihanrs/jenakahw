4bbf27b398176fa176d254cb5f3540a6eb9b0c13
vihanrs <vihanvrs@gmail.com>
2025-01-30 02:09:29 +0530

    add demo user logins

--- src/main/java/com/jenakahw/controller/BrandController.java
+++ src/main/java/com/jenakahw/controller/BrandController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jenakahw.domain.Brand;
-import com.jenakahw.repository.BrandRepository;
 import com.jenakahw.service.interfaces.BrandService;
 
 
